3e7de111235482e4ed7bea4bbe2821aa09932113
Adrià Lopez <calm@Adrias-MacBook-Pro.local>
2018-02-04 17:53:19 +0100

    Implementing a dynamic supervisor as an event manager

--- credotest/lib/event_manager.ex
+++ credotest/lib/event_manager.ex
@@ -4,7 +4,34 @@ defmodule CredoTest.EventManager do
   """
 
   def start_link() do
-    :hello
+    DynamicSupervisor.start_link(strategy: :one_for_one, name: :evt_manager)
+  end
+
+  def notify(evt) do
+    for {_, pid, _, _} <- DynamicSupervisor.which_children(:evt_manager) do
+      GenServer.cast(pid, evt)
+    end
+    :ok
+  end
+
+  def call(evt) do
+    :evt_manager
+    |> DynamicSupervisor.which_children()
+    |> Enum.map(&Task.async(GenServer, :call, [elem(&1, 1), evt]))
+    |> Enum.map(&Task.await(&1))
+  end
+
+ # testing this method. are all the ids in a DynamicSupervisor :undefined?
+  def terminate_handler(id) do
+    for {a, pid, _, _} <- DynamicSupervisor.which_children(:evt_manager), a == id  do
+      DynamicSupervisor.terminate_children(:evt_manager, pid)
+    end
+  end
+
+  def terminate_all_handlers() do
+    for {_id, pid, _, _} <- DynamicSupervisor.which_children(:evt_manager) do
+      DynamicSupervisor.terminate_children(:evt_manager, pid)
+    end
   end
 
 end
