d9ebe7a22c1659bdc2178cff33648e4dd4654b42
Adrià Lopez <calm@Adrias-MacBook-Pro.local>
2018-05-25 23:40:40 +0200

    My Axis project

---
+++ axis/lib/axis.ex
@@ -0,0 +1,18 @@
+defmodule Axis do
+  @moduledoc """
+  Documentation for Axis.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Axis.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ axis/test/axis_test.exs
@@ -0,0 +1,8 @@
+defmodule AxisTest do
+  use ExUnit.Case
+  doctest Axis
+
+  test "greets the world" do
+    assert Axis.hello() == :world
+  end
+end
